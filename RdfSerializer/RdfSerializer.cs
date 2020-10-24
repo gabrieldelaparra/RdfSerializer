0a1cb5989dc6924315670b8f59f6b9595091426d
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-10-24 19:41:31 +0200

    Add base project

---
+++ RdfSerializer/RdfSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace RdfSerializer
+{
+    public static class RdfSerializer
+    {
+        public static string DefaultIri { get; set; } = @"http://Example.rdf/";
+
+        public static IEnumerable<string>
+    }
+}
